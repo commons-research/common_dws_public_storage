5c82ffd99d57eb701354d1fe5cf542adbd49d74e
marco.visani@unifr.ch <marco.visani@unifr.ch>
2023-02-14 15:49:35 +0100

    add model to repo

--- docs/anticipated_lotus/model/move_to_dendron.sh
+++ docs/anticipated_lotus/model/move_to_dendron.sh
@@ -2,8 +2,7 @@
 
 pandoc -s Metabolites.tex -o model.md
 
-cd ../commons-dws-public
-lines=$(head -n 9 vault/open-notebook.commons.mvisani.model.md) ;
-echo "$lines" > vault/open-notebook.commons.mvisani.model.md
+lines=$(head -n 9 /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md) ;
+echo "$lines" > /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md
 
-cat ../model/model.md >> ./vault/open-notebook.commons.mvisani.model.md
+cat model.md >> /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md
