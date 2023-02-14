5ba4d45d8c2d1bad6986a9d0bf0c586024a3b06c
marco.visani@unifr.ch <marco.visani@unifr.ch>
2023-02-14 17:40:43 +0100

    updaily

--- docs/anticipated_lotus/model/move_to_dendron.sh
+++ docs/anticipated_lotus/model/move_to_dendron.sh
@@ -2,7 +2,7 @@
 
 pandoc -s Metabolites.tex -o model.md
 
-lines=$(head -n 9 /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md) ;
+lines=$(head -n 14 /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md) ;
 echo "$lines" > /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md
 
 cat model.md >> /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md
