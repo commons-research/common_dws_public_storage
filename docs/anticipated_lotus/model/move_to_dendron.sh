5a2dce18c04bc3849dc486ba852da41eb43f5a0a
marco.visani@unifr.ch <marco.visani@unifr.ch>
2023-02-24 15:32:38 +0100

    keep only necessary files

--- docs/anticipated_lotus/model/move_to_dendron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash zsh
-
-pandoc -s Metabolites.tex -o model.md
-
-lines=$(head -n 14 /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md) ;
-echo "$lines" > /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md
-
-cat model.md >> /Users/Marco/master_thesis/commons-dws-public/vault/anticipated-lotus.model.md
